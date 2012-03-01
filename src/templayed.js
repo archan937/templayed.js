cceb1e4c01fa7f987fc213cad028f60b08e7bdd9
Paul Engel <paul.engel@holder.nl>
2012-03-01 22:11:01 +0100

    Supporting dot variables, paths, functions, inverted sections, scoped variables and undefined variables

--- src/templayed.js
+++ src/templayed.js
@@ -11,31 +11,47 @@ if (typeof(Templayed) == "undefined") {
 // * $Date: {date} $
 // *
 
-Templayed = (function(undefined) {
-  var self = this;
+Templayed = (function() {
 
-  var render = function(template, variables) {
-    return template.replace(/{{#(.*?)}}(.*?){{\/\1}}/g, function(match, key, context) {
-      var array = variables[key];
-      var string = "";
-      for (var i = 0; i < array.length; i++) {
-        string += templayed(context, array[i]);
+  var render = function(template, vars) {
+    (vars instanceof Array) || (vars = [vars]);
+    return template.replace(/{{(\^|#)(.*?)}}(.*?){{\/\2}}/g, function(match, operator, key, context) {
+      var string = "", array = fetch(key, vars), dup, i;
+      if (operator == "^") {
+        return array.length ? string : render(context, vars);
+      }
+      for (i in array) {
+        dup = vars.slice();
+        dup.unshift(array[i]);
+        string += render(context, dup);
       }
       return string;
-    }).replace(/{{(!)?(.*?)}}+/g, function(match, operator, context) {
+    }).replace(/{{(!|#)?\s*(.*?)\s*}}+/g, function(match, operator, context) {
       switch (operator) {
       case "!":
         return "";
+      case "#":
+        return fetch(context, vars).apply(vars[0]);
       default:
-        return variables[context];
+        return context == "." ? vars[0] : fetch(context, vars);
       }
     });
+  },
+
+  fetch = function(path, vars) {
+    if (path.match(/\./)) {
+      var keys = path.split(".");
+      return fetch(keys.slice(1).join("."), [fetch(keys[0], vars)]);
+    } else {
+      return vars.length ? (vars[0].hasOwnProperty(path) ? vars[0][path] : fetch(path, vars.slice(1))) : "";
+    }
   };
 
   return {
     version: "{version}",
     render: render
   };
+
 }());
 
 templayed = function() {
