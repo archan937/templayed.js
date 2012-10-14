f1edd8ebbd369330a162530a24f8f84d7b9f7f4a
Paul Engel <paul.engel@holder.nl>
2012-10-14 11:42:50 +0200

    Added some corrections conform Mustache whitespace specs

--- src/templayed.js
+++ src/templayed.js
@@ -21,23 +21,23 @@ templayed = function(template, vars) {
   }, tag = function(template) {
     return template.replace(/((^|(\\r)?\\n)\s*)?\{\{(!|&|\{|>)?\s*(.*?)\s*}?}}(\s*(\\r)?\\n)?/g, function(match, b, s, r, operator, context, e) {
       if (operator == "!") return b && e ? s : [s, e].join('');
-      if (operator == ">") return [b, '" + (pars["', context, '"] ? templayed(pars["', context, '"])(vars[vars.length - 1], pars) : "") + "', e].join('');
+      if (operator == ">") return [s, '" + (pars["', context, '"] ? templayed(pars["', context, '"])(vars[vars.length - 1], pars) : "") + "', b && e ? '' : e].join('');
       var i = inc++;
       return [b, '"; var o', i, ' = ', get(context), ', s', i, ' = (((typeof(o', i, ') == "function" ? o', i, '.call(vars[vars.length - 1]) : o', i, ') || "") + ""); s += ',
         (operator ? ('s' + i) : '(/[&"><]/.test(s' + i + ') ? s' + i + '.replace(/&/g,"&amp;").replace(/"/g,"&quot;").replace(/>/g,"&gt;").replace(/</g,"&lt;") : s' + i + ')'), ' + "', e
       ].join('');
     });
   }, block = function(template) {
-    return tag(template.replace(/\{\{(\^|#)(.*?)}}(.*?)\{\{\/\2}}(\\n)?/g, function(match, operator, key, context) {
+    return tag(template.replace(/((^|(\\r)?\\n)\s*)?\{\{(\^|#)(.*?)}}(\s*(\\r)?\\n)?(.*?)((^|(\\r)?\\n)\s*)?\{\{\/\5}}(\s*(\\r)?\\n)?/g, function(match, b1, s1, r1, operator, key, e1, er1, context, b2, s2, r2, e2) {
       var i = inc++;
-      return ['"; var o', i, ' = ', get(key), '; ',
+      return [(b1 && e1 ? s1 : [s1, e1].join('')), '"; var o', i, ' = ', get(key), '; ',
         (operator == "^" ?
           ['if ((o', i, ' instanceof Array) ? !o', i, '.length : !o', i, ') { s += "', block(context), '"; } '] :
           ['if (typeof(o', i, ') == "boolean" && o', i, ') { s += "', block(context), '"; }',
            'else if (typeof(o', i, ') == "function") { s += o', i, '("', context.replace(/\{\{(\{?)/g, function(m, a) { return '\\{\\{' + (a ? '\\{' : ''); }), '"); }',
            'else if (o', i, ') { if (!(o', i, ' instanceof Array)) o', i, ' = [o', i, ']; for (var i', i, ' = 0; i', i, ' < o', i, '.length; i', i, '++) {',
             'vars.push(o', i, '[i', i, ']); s += "', block(context), '"; vars.pop(); }}']
-        ).join(''), '; s += "', ].join('');
+        ).join(''), '; s += "', (b2 && e2 ? s2 : [s2, e2].join(''))].join('');
     })).replace(/\\\{{2}(\\\{?)/g, function(m, a) { return '{{' + (a ? '{' : ''); });
   }, inc = 0;
 
